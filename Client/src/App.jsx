07b66cd81bf0dd5002d1221831884bf631a89e3d
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2025-01-02 19:33:17 +0530

    title added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,13 +11,14 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 
 
 function App() {
-  const endpoint = "https://solana-devnet.g.alchemy.com/v2/wtp7XKCz0CbtY5HEJE4ntow9f2oA96mn"//customised RPC
+  const endpoint = "https://solana-devnet.g.alchemy.com/v2/wtp7XKCz0CbtY5HEJE4ntow9f2oA96mn"
 
   return (
     <div className='max-w-screen-xl mx-auto p-8 text-center'>
       <ConnectionProvider endpoint={endpoint}>
         <WalletProvider wallets={[]} autoConnect>
           <WalletModalProvider>
+            <h1 className='text-[#a99df1]'>Solana Faucet</h1>
             <div className='flex gap-10'>
               <WalletMultiButton />
               <WalletDisconnectButton />
